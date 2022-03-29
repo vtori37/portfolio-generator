efc62dc558e9f0fa3c39c6ae77fbf1ee3af059c9
Tori Acer <vrice360@gmail.com>
2022-03-29 16:43:42 -0500

    edits to capture user inputs

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 var message = 'Hello Node!';
 
-var sum = 5 + 3;
-
-console.log(message);
-console.log(sum);
-
-var commandLinesArgs = process.argv;
-console.log(commandLinesArgs);
-
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
-// / Notice the lack of parentheses around the `profileDataArr` parameter?
 const printProfileData = profileDataArr => {
   // This...
   for (let i = 0; i < profileDataArr.length; i += 1) {
