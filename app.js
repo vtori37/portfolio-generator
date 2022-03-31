e2092f2f82b30488c88c21482ff2715f8c9e281b
Tori Acer <vrice360@gmail.com>
2022-03-31 15:59:05 -0500

    interfaced with inquirer package

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js'); 
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
-
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type:'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+.then(answers => console.log(answers));
 
 
 
 
 
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js'); 
+
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 
-
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
 
