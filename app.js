66ce5178c354958eab4a9895c9d74096eb47f616
Tori Acer <vrice360@gmail.com>
2022-03-31 16:34:09 -0500

    finished adding questions

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const inquirer = require('inquirer');
 const promptUser = () => {
   return inquirer.prompt([
     {
+      
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      },
     },
     {
       type: 'input',
@@ -13,10 +22,23 @@ const promptUser = () => {
       message: 'Enter your GitHub Username'
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
       type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:'
+  name: 'about',
+  message: 'Provide some information about yourself:',
+  when: ({ confirmAbout }) => {
+    if (confirmAbout) {
+      return true;
+    } else {
+      return false;
     }
+  }
+}
   ]);
 };
 
