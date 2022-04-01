a7e846b22764153d9514c5d5f5f7584fc4516531
Tori Acer <vrice360@gmail.com>
2022-04-01 17:40:58 -0500

    updated template

--- app.js
+++ app.js
@@ -60,7 +60,8 @@ if (!portfolioData.projects) {
   portfolioData.projects = [];
 }
 
-return inquirer.prompt([
+return inquirer
+  .prompt([
     {
       type: 'input',
       name: 'name',
@@ -134,24 +135,13 @@ promptUser()
   .then(portfolioData => {
     const pageHTML = generatePage(portfolioData);
 
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
 
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
 
-// promptUser().then(answer => console.log(answers));
-
-
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 
 
